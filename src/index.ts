341743929ca9e110b7ddd9c41479e0df283d70be
Shamir Sas <shamirsas@outlook.com>
2026-05-05 14:22:30 +0200

    Added .graphql file extension support

--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,26 @@
-import { ApolloApp } from "./apollo-app.js";
+import { loadSchemaSync } from "@graphql-tools/load";
+import { ApolloApp } from "./apollo-app";
+import { GraphQLFileLoader } from "@graphql-tools/graphql-file-loader";
+import { addResolversToSchema } from "@graphql-tools/schema";
+import { resolvers } from "./resolvers";
 
-const books = [
-  {
-    title: "The Awakening",
-    author: "Kate Chopin",
-  },
-  {
-    title: "City of Glass!!!!",
-    author: "Paul Austeraascascasc!",
-  },
-];
-
-const typeDefs = `#graphql
-  type Book {
-    title: String
-    author: String
-  }
-
-  type Query {
-    books: [Book]
-  }
-`;
-
-const resolvers = {
-  Query: {
-    books: () => books,
-  },
-};
-
-const { url } = await ApolloApp.startStandaloneServer(9000, {
-  typeDefs,
-  resolvers,
+const schema = loadSchemaSync("./src/modules/**/*.graphql", {
+  loaders: [new GraphQLFileLoader()],
 });
-console.log(`🚀  Server ready at: ${url}`);
+
+const schemaWithResolvers = addResolversToSchema({
+  schema,
+  resolvers: resolvers,
+});
+
+async function main(): Promise<void> {
+  const { url } = await ApolloApp.startStandaloneServer(4000, {
+    schema: schemaWithResolvers,
+  });
+  console.log(`🚀  Server ready at: ${url}`);
+}
+
+main().catch((err: unknown) => {
+  console.error(err);
+  process.exit(1);
+});
