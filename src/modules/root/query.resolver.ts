8602c71820519f908873a7639ac3a7cadd4b5e13
Shamir Sas <shamirsas@outlook.com>
2026-05-06 14:13:12 +0200

    Implemented unit tests for resolver loading mechanism and enhanced class validation utilities. Updated existing tests to ensure compatibility with recent changes.

--- src/modules/root/query.resolver.ts
+++ src/modules/root/query.resolver.ts
@@ -1,7 +1,15 @@
+import { Resolver } from "../../shared/classes";
 import { LocationSearchResponse } from "../../shared/http/integrations/location-http-client.interface";
 import { LocationLib } from "../location";
 import { RankingLib, OrderedRankingActivities } from "../ranking";
-export class QueryResolver {
+
+export class QueryResolver extends Resolver {
+  /**
+   * Get the locations for a given name.
+   * @param _ - The root object.
+   * @param {string} name - The name of the location.
+   * @returns The locations.
+   */
   public getLocations(
     _: any,
     { name }: { name: string },
@@ -9,6 +17,13 @@ export class QueryResolver {
     return LocationLib.getLocations(name);
   }
 
+  /**
+   * Get the activities ranking for a given longitude and latitude.
+   * @param _ - The root object.
+   * @param {number} longitude - The longitude of the location.
+   * @param {number} latitude - The latitude of the location.
+   * @returns The ranking.
+   */
   public getRanking(
     _: any,
     {
